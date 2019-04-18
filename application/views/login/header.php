e336f66a6724ed1356caa8ee4c89fc37faeebc9f
andrezjimenez <andru456@hotmail.com>
2019-04-18 04:26:58 -0500

    front login

---
+++ application/views/login/header.php
@@ -0,0 +1,29 @@
+<html>
+	<head>	
+		<!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">-->
+		
+		<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+		<!--===============================================================================================-->
+			<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/login/vendor/bootstrap/css/bootstrap.min.css">
+		<!--===============================================================================================-->
+			<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+		<!--===============================================================================================-->
+			<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/login/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+		<!--===============================================================================================-->
+			<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/login/vendor/animate/animate.css">
+		<!--===============================================================================================-->	
+			<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/login/vendor/css-hamburgers/hamburgers.min.css">
+		<!--===============================================================================================-->
+			<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/login/vendor/animsition/css/animsition.min.css">
+		<!--===============================================================================================-->
+			<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/login/vendor/select2/select2.min.css">
+		<!--===============================================================================================-->	
+			<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/login/vendor/daterangepicker/daterangepicker.css">
+		<!--===============================================================================================-->
+			<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/login/css/util.css">
+			<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/login/css/main.css">
+		
+		<title>Funeraria los Olivos</title>
+	</head>
+	<body>
+
---
+++ application/views/login/login.php
@@ -0,0 +1,45 @@
+<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form">
+					<span class="login100-form-title p-b-43">
+						Bienvenido al portal del Afiliacion
+					</span>
+					
+					
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="email">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Usuario</span>
+					</div>
+					
+					
+					<div class="wrap-input100 validate-input" data-validate="Password is required">
+						<input class="input100" type="password" name="pass">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Password</span>
+					</div>
+
+					<div class="flex-sb-m w-full p-t-3 p-b-32">
+						<div>
+							<a href="#" class="txt1">
+								Registrarse
+							</a>
+						</div>
+					</div>
+			
+
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Iniciar Sesion 
+						</button>
+					</div>
+
+				</form>
+				
+				<div class="login100-more" style="background-image: url('<?php echo base_url();?>assets/login/img/013.jpg');">
+				</div>
+			</div>
+		</div>
+	</div>
+	
